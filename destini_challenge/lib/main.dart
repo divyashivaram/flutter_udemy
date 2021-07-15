93c93dd16bc7dac8613dc485ad480c5d3044e555
Divya <div.shivaram@gmail.com>
2021-07-15 19:11:55 +0530

    destini: todo 1: add background image

--- destini_challenge/lib/main.dart
+++ destini_challenge/lib/main.dart
@@ -24,9 +24,13 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+          ),
+        ),
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
